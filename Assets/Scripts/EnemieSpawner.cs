b0e98710a0dc58cf92608e2e2baff6d2d3d20361
Sebastiran <Sebastiaan.vandijk@hotmail.com>
2015-01-23 22:55:36 +0100

    EnemySpawner added

---
+++ Assets/Scripts/EnemieSpawner.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemieSpawner : MonoBehaviour {
+
+	public float time = 2;
+	public float timer;
+	public float timerOffset = 1;
+	int lane;
+	float enemyXPos;
+	
+	public GameObject[] enemies;
+
+	void Start()
+	{
+		enemyXPos = Camera.main.transform.position.x + (Camera.main.pixelWidth / 2);
+		SetTimer ();
+	}
+
+	void Update () {
+		timer = timer - Time.deltaTime;
+		if(timer <= 0)
+		{
+			//Instanciate
+			lane = getLane();
+			float enemyYPos = lane;
+			Vector3 enemyPos = new Vector3(enemyXPos, enemyYPos, 0);
+			GameObject enemyPrefab = getEnemy();
+			GameObject spawnedEnemy = (GameObject)Instantiate(enemyPrefab, enemyPos, enemyPrefab.transform.rotation);
+			SetTimer();
+		}
+	}
+
+	void SetTimer()
+	{
+		timer = Random.Range (time - timerOffset, time + timerOffset);
+	}
+
+	GameObject getEnemy()
+	{
+		return enemies [Random.Range (0, enemies.Length - 1)];
+	}
+
+	int getLane()
+	{
+		return Random.Range (0, 5);
+	}
+}
