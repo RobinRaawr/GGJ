456a9dc7627ea00253ff6e341d05bea498e20c15
Sebastiran <Sebastiaan.vandijk@hotmail.com>
2015-01-25 15:06:56 +0100

    Gui works

--- Assets/Assets/Scripts/DontDestroyOnLoad.cs
+++ Assets/Assets/Scripts/DontDestroyOnLoad.cs
@@ -3,8 +3,18 @@ using System.Collections;
 
 public class DontDestroyOnLoad : MonoBehaviour {
 
-    void Start()
-    {
-        DontDestroyOnLoad(this.gameObject);
-    }
+	public static DontDestroyOnLoad a;
+
+	void Awake()
+	{
+		if(a == null)
+		{
+			DontDestroyOnLoad(gameObject);
+			a = this;
+		}
+		else if(a != this)
+		{
+			Destroy(gameObject);
+		}
+	}
 }
