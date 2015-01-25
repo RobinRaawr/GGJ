fa00b8a9bca980430e75500be7f2edda141b59f8
Robin Kok <Rkok94@gmail.com>
2015-01-25 11:11:09 +0100

    Gletjes9000 introduced. Gletsjer movement en particles

---
+++ Assets/Assets/Scripts/GletjserMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GletjserMovement : MonoBehaviour {
+
+    public float speed = 10f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position += new Vector3(speed, 0, 0);
+	
+	}
+}
---
+++ Assets/Assets/Scripts/GletsjerRespawn.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GletsjerRespawn : MonoBehaviour {
+
+    public GameObject spawnPoint;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Spawnpoint")
+        {
+            transform.position = new Vector3(spawnPoint.transform.position.x, transform.position.y, transform.position.z);
+        }
+
+    }
+}
